0302051d34decd579812a72df617451d0517d57c
u213197 <csajagi.matyas@mbhbank.hu>
2025-09-04 18:05:23 +0200

    Grrrrrrrr

--- src/test/java/seleniumTests/SeleniumBase.java
+++ src/test/java/seleniumTests/SeleniumBase.java
@@ -1,15 +1,17 @@
 package seleniumTests;
 
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.events.EventFiringDecorator;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import seleniumTests.helpers.SlowDownListener;
 import seleniumTests.seleniumActions.SeleniumActions;
 
+import java.io.File;
 import java.time.Duration;
 
 public class SeleniumBase {
@@ -19,7 +21,24 @@ public class SeleniumBase {
 
     @BeforeMethod
     public void openPage() {
-        ChromeDriver rawDriver = new ChromeDriver();
+
+        boolean headless = Boolean.parseBoolean(System.getenv("HEADLESS_MODE"));
+        ChromeOptions options = new ChromeOptions();
+
+        String chromeDriverVersion = "140.0.7339.80";
+        System.setProperty("webdriver.chrome.driver", downloadChromeDriver(chromeDriverVersion));
+
+        if (headless) {
+            options.addArguments("--headless");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--remote-debugging-port=9222");
+            options.addArguments("--window-size=1920x1080");
+            options.addArguments("--disable-software-rasterizer");
+        }
+
+        ChromeDriver rawDriver = new ChromeDriver(options);
         SlowDownListener listener = new SlowDownListener();
         WebDriver decoratedDriver = new EventFiringDecorator(listener).decorate(rawDriver);
 
@@ -32,6 +51,29 @@ public class SeleniumBase {
 
         seleniumActions = new SeleniumActions(driver);
         seleniumActions.closePopupsIfPresent();
+    }
+
+    private String downloadChromeDriver(String version) {
+        String downloadUrl = "https://chromedriver.storage.googleapis.com/" + version + "/chromedriver_linux64.zip";
+        String downloadDir = System.getProperty("user.dir") + "/chromedriver/";
+
+        try {
+            File dir = new File(downloadDir);
+            if (!dir.exists()) {
+                dir.mkdirs();
+            }
+
+            String wgetCommand = "wget -q " + downloadUrl + " -P " + downloadDir;
+            String unzipCommand = "unzip -q " + downloadDir + "chromedriver_linux64.zip -d " + downloadDir;
+
+            Runtime.getRuntime().exec(wgetCommand).waitFor();
+            Runtime.getRuntime().exec(unzipCommand).waitFor();
+
+            return downloadDir + "chromedriver";
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("ChromeDriver letöltés vagy telepítés nem sikerült.");
+        }
     }
 
     public WebDriver getDriver() {
