f38b58820f96fc0417f504c50084194495a4324a
u213197 <csajagi.matyas@mbhbank.hu>
2025-09-04 19:23:02 +0200

    Fixes bugs hopefully

--- src/test/java/seleniumTests/SeleniumBase.java
+++ src/test/java/seleniumTests/SeleniumBase.java
@@ -1,7 +1,6 @@
 package seleniumTests;
 
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.events.EventFiringDecorator;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.JavascriptExecutor;
@@ -11,8 +10,6 @@ import org.testng.annotations.BeforeMethod;
 import seleniumTests.helpers.SlowDownListener;
 import seleniumTests.seleniumActions.SeleniumActions;
 
-import java.io.File;
-import java.io.IOException;
 import java.time.Duration;
 
 public class SeleniumBase {
@@ -22,27 +19,9 @@ public class SeleniumBase {
 
     @BeforeMethod
     public void openPage() {
-
-        boolean headless = Boolean.parseBoolean(System.getenv("HEADLESS_MODE"));
-        ChromeOptions options = new ChromeOptions();
-
-        String chromeDriverVersion = "140.0.7339.80";
-        System.setProperty("webdriver.chrome.driver", downloadChromeDriver(chromeDriverVersion));
-
-        if (headless) {
-            options.addArguments("--headless");
-            options.addArguments("--no-sandbox");
-            options.addArguments("--disable-dev-shm-usage");
-            options.addArguments("--disable-gpu");
-            options.addArguments("--remote-debugging-port=9222");
-            options.addArguments("--window-size=1920x1080");
-            options.addArguments("--disable-software-rasterizer");
-        }
-
-        ChromeDriver rawDriver = new ChromeDriver(options);
+        driver = new ChromeDriver();
         SlowDownListener listener = new SlowDownListener();
-        WebDriver decoratedDriver = new EventFiringDecorator(listener).decorate(rawDriver);
-
+        WebDriver decoratedDriver = new EventFiringDecorator(listener).decorate(driver);
         this.driver = decoratedDriver;
         driver.get("https://gsfanatic.com");
 
@@ -52,46 +31,6 @@ public class SeleniumBase {
 
         seleniumActions = new SeleniumActions(driver);
         seleniumActions.closePopupsIfPresent();
-    }
-
-    private String downloadChromeDriver(String version) {
-        String downloadUrl = "https://chromedriver.storage.googleapis.com/" + version + "/chromedriver_linux64.zip";
-        String downloadDir = System.getProperty("user.dir") + "/chromedriver/";
-
-        try {
-            File dir = new File(downloadDir);
-            if (!dir.exists()) {
-                boolean created = dir.mkdirs();
-                if (created) {
-                    System.out.println("A könyvtár létrehozva: " + downloadDir);
-                } else {
-                    System.out.println("A könyvtár nem hozható létre: " + downloadDir);
-                }
-            }
-
-            String wgetCommand = "wget -q " + downloadUrl + " -P " + downloadDir;
-            String unzipCommand = "unzip -q " + downloadDir + "chromedriver_linux64.zip -d " + downloadDir;
-
-            Process wgetProcess = Runtime.getRuntime().exec(wgetCommand);
-            int wgetExitCode = wgetProcess.waitFor();
-            if (wgetExitCode != 0) {
-                System.out.println("Hiba a ChromeDriver letöltése közben!");
-            }
-
-            Process unzipProcess = Runtime.getRuntime().exec(unzipCommand);
-            int unzipExitCode = unzipProcess.waitFor();
-            if (unzipExitCode != 0) {
-                System.out.println("Hiba a ZIP fájl kicsomagolása közben!");
-            }
-
-            String chromedriverPath = downloadDir + "chromedriver";
-            System.out.println("ChromeDriver elérési útja: " + chromedriverPath);
-            return chromedriverPath;
-
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-            throw new RuntimeException("ChromeDriver letöltés vagy telepítés nem sikerült.");
-        }
     }
 
     public WebDriver getDriver() {
