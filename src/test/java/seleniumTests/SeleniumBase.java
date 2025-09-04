bb96bfad13137605f5061a1da54ac6b78bdbaf33
u213197 <csajagi.matyas@mbhbank.hu>
2025-09-04 17:56:33 +0200

    Leszedtem a Chromedrivert

--- src/test/java/seleniumTests/SeleniumBase.java
+++ src/test/java/seleniumTests/SeleniumBase.java
@@ -3,7 +3,6 @@ package seleniumTests;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.events.EventFiringDecorator;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
@@ -20,21 +19,7 @@ public class SeleniumBase {
 
     @BeforeMethod
     public void openPage() {
-
-        boolean headless = Boolean.parseBoolean(System.getenv("HEADLESS_MODE"));
-        ChromeOptions options = new ChromeOptions();
-
-        if (headless) {
-            options.addArguments("--headless");
-            options.addArguments("--no-sandbox");
-            options.addArguments("--disable-dev-shm-usage");
-            options.addArguments("--disable-gpu");
-            options.addArguments("--remote-debugging-port=9222");
-            options.addArguments("--window-size=1920x1080");
-            options.addArguments("--disable-software-rasterizer");
-        }
-
-        ChromeDriver rawDriver = new ChromeDriver(options);
+        ChromeDriver rawDriver = new ChromeDriver();
         SlowDownListener listener = new SlowDownListener();
         WebDriver decoratedDriver = new EventFiringDecorator(listener).decorate(rawDriver);
 
